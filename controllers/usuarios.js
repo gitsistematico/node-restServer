8616cad89e291928fcf45379431b5b331061ea52
RicardoRincon <ricardo.rincon@medismart.live>
2023-09-13 21:55:13 -0500

    last change 13092023

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,78 +1,75 @@
-const { response, request } = require('express');
-const Usuario = require('../models/usuario');
-const bcryptjs = require('bcryptjs');
+const { response, request } = require("express");
+const Usuario = require("../models/usuario");
+const bcryptjs = require("bcryptjs");
 
-const usuariosGet = async(req = request, res = response) => {
-    
-    const { limite = 5, desde = 0 } = req.query;
-    const query = { estado: true };
+const usuariosGet = async (req = request, res = response) => {
+  const { limite = 5, desde = 0 } = req.query;
+  const query = { estado: true };
 
-    const [ total, usuarios] = await Promise.all([
-        Usuario.countDocuments(query),
-        Usuario.find(query)
-            .skip(Number (desde))
-            .limit(Number (limite))
+  const [total, usuarios] = await Promise.all([
+    Usuario.countDocuments(query),
+    Usuario.find(query)
+           .skip(Number(desde))
+           .limit(Number(limite)),
+  ]);
+  res.json({
+    total,
+    usuarios,
+  });
+};
 
-    ])
-    res.json({
-        total, usuarios
-    });
-}
+const usuariosPut = async (req, res = response) => {
+  const { id } = req.params;
+  const { _id, password, google, correo, ...resto } = req.body;
 
-const usuariosPut = async(req , res = response) => {
+  if (password) {
+    // Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    resto.password = bcryptjs.hashSync(password, salt);
+  }
 
-    const { id } = req.params;
-    const { _id, password, google, correo, ...resto } = req.body;
+  const usuario = await Usuario.findByIdAndUpdate(id, resto);
 
-    if ( password ) {
-        // Encriptar la contraseña
-        const salt = bcryptjs.genSaltSync();
-        resto.password = bcryptjs.hashSync( password, salt );
-    }
-
-    const usuario = await Usuario.findByIdAndUpdate( id, resto );
-
-    res.json(usuario);
-}
+  res.json(usuario);
+};
 
 const usuariosPost = async (req = request, res = response) => {
-    
-    const { nombre, correo, password, rol } = req.body;
-    const usuario = new Usuario({ nombre, correo, password, rol });
-    //encriptar la contraseña
-    const  salt = bcryptjs.genSaltSync();
-    usuario.password = bcryptjs.hashSync( password, salt );
+  const { nombre, correo, password, rol } = req.body;
+  const usuario = new Usuario({ nombre, correo, password, rol });
+  //encriptar la contraseña
+  const salt = bcryptjs.genSaltSync();
+  usuario.password = bcryptjs.hashSync(password, salt);
 
-    //Guardar en BD
-    await usuario.save();
-    res.json({        
-        usuario
-    });
-}
+  //Guardar en BD
+  await usuario.save();
+  res.json({
+    usuario,
+  });
+};
 
-const usuariosDelete =  async (req , res = response) => {
-    const { id } =  req.params;
-    const uid = req.uid;
-    
-    const usuario = await Usuario.findByIdAndUpdate( id, { estado: false });
-    // Usuario Autenticado
-    // const usuarioAutentucado = req.usuarioAuth;
+const usuariosDelete = async (req, res = response) => {
+  const { id } = req.params;
+  const uid = req.uid;
 
-    res.json({
-        usuario                
-    });
-}
+  const usuario = await Usuario.findByIdAndUpdate(id, { estado: false });
+  // Usuario Autenticado
+  // const usuarioAutentucado = req.usuarioAuth;
+
+  res.json({
+    usuario,
+  });
+};
 
 const usuariosPatch = (req = request, res = response) => {
-    res.json({
-        msg: 'get Patch *- controlador'
-    });
-}
+  res.json({
+    msg: "get Patch *- controlador",
+  });
+};
 
 module.exports = {
-    usuariosGet,
-    usuariosPut,
-    usuariosPost,
-    usuariosDelete,
-    usuariosPatch
-}
+  usuariosGet,
+  usuariosPut,
+  usuariosPost,
+  usuariosDelete,
+  usuariosPatch,
+};
--- database/confing.js
+++ database/confing.js
@@ -1,18 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
-const dbConnection = async() => {
-
-    try {
-        
-        await mongoose.connect( process.env.MONGODB_CNN);
-        console.log("Base de datos Online");
-    } catch (error) {
-        console.log(error);
-        throw new Error('Error a la hora de iniciar la base de datos');
-    }
-}
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_CNN
+    //     , {
+    //   useNewUrlParse: true,
+    //   useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: false,
+    // }
+    );
+    console.log("Base de datos Online");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error a la hora de iniciar la base de datos");
+  }
+};
 
 module.exports = {
-    dbConnection
-}
+  dbConnection,
+};
